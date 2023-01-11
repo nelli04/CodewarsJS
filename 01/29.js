678432d95096c80ed9b3113d85c7b4baf4a38880
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-11 19:31:22 +0300

    f

---
+++ 01/29.js
@@ -0,0 +1,25 @@
+function countGrade(scores){
+    const obj = {S:0, A:0, B:0, C:0, D:0, X:0}
+    scores.map( v => {
+    if(v >= 100) {
+      obj[`S`]++
+    }
+    if(v >= 90 && v < 100) {
+      obj[`A`]++
+    }
+    if(v >= 80 && v < 90) {
+      obj[`B`]++
+    }
+    if(v >= 60 && v < 80) {
+      obj[`C`]++
+    }
+    if(v >= 0 && v < 60) {
+      obj[`D`]++
+    }
+    if(v < 0) {
+      obj[`X`]++
+    }
+      
+    })
+    return obj
+  }
