922817faff39744ef92860ec7c988accabffa72a
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-13 15:45:32 +0300

    f

---
+++ 02/Roman Numerals Encoder.js
@@ -0,0 +1,12 @@
+function solution(number){
+    const x = [['M', 1000], ['CM', 900], ['D', 500], ['CD', 400], ['C', 100], ['XC', 90], ['L', 50], ['XL', 40], ['X', 10], ['IX', 9], ['V', 5], ['IV', 4], ['I', 1]];
+    var y = [];
+    x.forEach(function(z) {
+      while (number >= z[1]) {
+        y.push(z[0]);
+        number -= z[1];
+      }
+    });
+    return y.join('');
+  }
+  
