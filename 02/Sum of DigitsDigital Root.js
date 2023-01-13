63458b08cba7c6fd405468d035485d8ecbe530f3
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-01-13 14:42:26 +0300

    f

---
+++ 02/Sum of DigitsDigital Root.js
@@ -0,0 +1,12 @@
+function digitalRoot(n) {
+    if (n < 10) {
+      return n;
+    }
+  
+    while (n > 9) {
+      const x = String(n).split('').map(Number);
+      n = x.reduce((y, z) => y + z);
+    }
+    console.log('n - ',n);
+    return n;
+  }
