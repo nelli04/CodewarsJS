ce8bb889eb2278f4b0ebb9f59da266496ac70d46
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-20 21:56:08 +0300

    c

--- 02/Exclusive (xor) Logical Operator.js
+++ 02/Exclusive (xor) Logical Operator.js
@@ -1,3 +1,14 @@
 function xor(a, b) {
     return a !== b
   }
+
+  //other solution 
+  function xor(a, b) {
+    if ((a === true) && (b !== true)){
+    return true
+    } else if ((a !== true) && (b === true)){
+    return true
+    } else {
+    return false
+    }
+  }
