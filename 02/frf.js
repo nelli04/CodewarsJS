2c7abaab73c019cbd5c94b80d4df166308072ecf
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-20 22:12:07 +0300

    c

--- 02/frf.js
+++ 02/frf.js
@@ -11,4 +11,11 @@ function noIfsNoButs(a, b) {
         break;
     }
   }
-  
+  //other solution
+  var noIfsNoButs = function (a,b) {
+    switch (Math.sign(a-b)) {
+      case 1: return `${a} is greater than ${b}`;
+      case -1:return `${a} is smaller than ${b}`;
+      case 0: return `${a} is equal to ${b}`;
+    }
+  }
