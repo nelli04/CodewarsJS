9ddb21a750c5ae2908bb9a96d68552b266ee650d
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-20 22:11:08 +0300

    c

---
+++ 02/frf.js
@@ -0,0 +1,14 @@
+function noIfsNoButs(a, b) {
+    switch(true) {
+        case (a == b): 
+        return a.toString() + ' is equal to ' + b.toString()
+        break;
+        case (a > b):
+        return a.toString() + ' is greater than ' + b.toString()
+        break;
+        case (a < b): 
+        return a.toString() + ' is smaller than ' + b.toString()
+        break;
+    }
+  }
+  
