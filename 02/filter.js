16e6dbf32d91f7725f066bf23da09376843fad12
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-28 15:47:59 +0300

    c

--- 02/filter.js
+++ 02/filter.js
@@ -1,6 +1,6 @@
 function filter_list(l) {
     var integers = []
-    for(var i = 0; i < l.length; i++){
+    for(let i = 0; i < l.length; i++){
     if(Number.isInteger(l[i])){
     integers.push(l[i])
       } 
