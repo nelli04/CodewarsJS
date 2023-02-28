e479ce1e76a0baf0e6599159588b4c640f3c2e88
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-02-28 15:46:06 +0300

    c

---
+++ 02/filter.js
@@ -0,0 +1,14 @@
+function filter_list(l) {
+    var integers = []
+    for(var i = 0; i < l.length; i++){
+    if(Number.isInteger(l[i])){
+    integers.push(l[i])
+      } 
+    }
+    return integers
+  }
+
+  //other solution
+  function filter_list(l) {
+    return l.filter(f => typeof f == 'number')
+  }
