68cfe9bab2cba75edfeb548df2c4f876c38bc4c2
Nelli_Ivanova <115640860+nelli04@users.noreply.github.com>
2023-06-07 20:57:50 +0300

    Update Sum of DigitsDigital Root.js

--- 02/Sum of DigitsDigital Root.js
+++ 02/Sum of DigitsDigital Root.js
@@ -1,8 +1,5 @@
 function digitalRoot(n) {
-    if (n < 10) {
-      return n;
-    }
-  
+    if (n < 10) return n;
     while (n > 9) {
       const x = String(n).split('').map(Number);
       n = x.reduce((y, z) => y + z);
